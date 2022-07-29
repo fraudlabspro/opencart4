a8e136cf3d29f2116564f9c541a34ad5f82e4f10
FraudLabs Pro <support@fraudlabspro.com>
2022-07-29 10:19:17 +0800

    Support OpenCart 4

---
+++ README.md
@@ -0,0 +1,48 @@
+# FraudLabs Pro Fraud Prevention OpenCart extension
+FraudLabs Pro Fraud Prevention extension to help merchants to screen the order transactions for payment fraud. Its comprehensive and advanced algorithm engines validate all elements such as geolocation, anonymous proxy, email address, blacklist record, credit card BIN, transaction velocity and many more to pinpoint fraud orders accurately.
+
+Free Micro Plan available with 500 validations per month.
+
+Please visit the [FraudLabs Pro Articles & Tutorials](https://www.fraudlabspro.com/resources/) to learn more about our fraud validation.
+
+*Note: This extension works in OpenCart 4.*
+
+## Why FraudLabs Pro
+* **Flexibility**
+
+We offer you a flexible solution to identify a fraud order. You can use our FraudLabs Pro score to determine a fraud order or customize your own validation rules to target a specific case, or a combination of both.
+
+* **Free**
+
+We are not offering you a trial version, but a free version to start protecting your online business. It’s absolutely free if your monthly orders are less than 500 transactions. There is no upfront credit card information needed, commitment, hidden cost and whatsoever.
+
+* **Trustworthy**
+
+We have been in the fraud prevention industry for more than 10 years. Thousands of our clients are currently using our FraudLabs Pro solution to protect their e-commerce sites from fraud.
+
+* **Key Features**
+  * Fraud analysis and scoring
+  * IP address geolocation & proxy validation
+  * Email address validation
+  * Transaction velocity validation
+  * Device transaction validation
+  * Blacklist validation
+  * Custom validation rules
+  * FraudLabs Pro Merchant Network
+  * Email notification of fraud orders
+  * Mobile App (Android & iOS) notification of fraud orders
+  * Support SMS Verification to further validate the fraudulent order
+
+## Installation instructions
+1. Download the extension archive.
+1. Unzip the extension archive.
+1. Login to the OpenCart admin area.
+1. Go to Extensions > Installer and upload the fraudlabspro.ocmod.zip file.
+1. Go to Extensions > Extensions > Anti-Fraud > FraudLabs Pro to install and edit the settings of this extension.
+1. Select Enabled. Then enter the FraudLabs Pro API key and other settings.
+1. Click on the save button to save the new configuration.
+
+## SUPPORT
+Email: support@fraudlabspro.com
+
+Please visit us at [FraudLabs Pro](https://www.fraudlabspro.com/supported-platforms/opencart/) for more information.
---
+++ admin/language/en-gb/fraud/fraudlabspro.php
@@ -0,0 +1,84 @@
+<?php
+// Heading
+$_['heading_title']              = 'FraudLabs Pro';
+
+// Text
+$_['text_extension']             = 'Extensions';
+$_['text_success']               = 'Success: You have modified FraudLabs Pro Settings!';
+$_['text_success_delete']        = 'Success: You have deleted all FraudLabs Pro data!';
+$_['text_edit']                  = 'Settings';
+$_['text_signup']                = 'FraudLabs Pro is a fraud detection solution. It helps you to reduce your fraud losses or chargebacks by screening all your incoming orders for fraud patterns. You can <u><a href="https://www.fraudlabspro.com/pricing?ref=1730" target="_blank" class="alert-link">sign up</a></u> for a free Micro plan to get the API key if you do not have one.';
+$_['text_rule_info']             = 'Please visit <strong>Rules</strong> settings page on <a href="https://www.fraudlabspro.com/merchant/rule" target="_blank">Merchant Area</a> to review or edit the fraud score threshold and the associated action.';
+$_['text_information']             = 'Please visit <a href="https://www.fraudlabspro.com/resources/categories/opencart" target="_blank">FraudLabs Pro Articles & Tutorials</a> page to learn more about the services.<br/><br/>Please <a href="https://www.fraudlabspro.com/contact" target="_blank">contact us</a> if any help is needed.';
+$_['text_id']                    = 'FraudLabs Pro ID';
+$_['text_ip_address']            = 'IP Address';
+$_['text_ip_usage_type']         = 'IP Usage Type';
+$_['text_ip_time_zone']          = 'IP Time Zone';
+$_['text_ip_location']           = 'IP Location';
+$_['text_ip_distance']           = 'IP to Billing Distance';
+$_['text_ip_latitude']           = 'IP Latitude';
+$_['text_ip_longitude']          = 'IP Longitude';
+$_['text_risk_country']          = 'High Risk Country';
+$_['text_free_email']            = 'Free Email Domain';
+$_['text_ship_forward']          = 'Ship Fordwarder';
+$_['text_using_proxy']           = 'Proxy IP Address';
+$_['text_ip_blacklist']          = 'IP in Blacklist';
+$_['text_email_blacklist']       = 'Email in Blacklist';
+$_['text_phone_verify']          = 'Phone Verified';
+$_['text_score']                 = 'FraudLabs Pro Score';
+$_['text_status']                = 'FraudLabs Pro Status';
+$_['text_message']               = 'Message';
+$_['text_transaction_id']        = 'Transaction ID';
+$_['text_rules']                 = 'Triggered Rules';
+$_['text_credits']               = 'Credit Balance';
+$_['text_error']                 = 'Error:';
+$_['text_flp_upgrade']           = '<a href="https://www.fraudlabspro.com/pricing" target="_blank">[Upgrade]</a>';
+$_['text_zapier_approve']        = 'Approve Status';
+$_['text_zapier_review']         = 'Review Status';
+$_['text_zapier_reject']         = 'Reject Status';
+
+// Entry
+$_['entry_status']               = 'Status';
+$_['entry_key']                  = 'API Key';
+$_['entry_score']                = 'Risk Score';
+$_['entry_order_status']         = 'Order Status';
+$_['entry_review_status']        = 'Review Status';
+$_['entry_approve_status']       = 'Approve Status';
+$_['entry_reject_status']        = 'Reject Status';
+$_['entry_email_notification']   = 'Email Notification';
+$_['entry_zapier_notification']  = 'Zapier Notification';
+$_['entry_simulate_ip']          = 'Simulate IP';
+$_['entry_debug']                = 'Save Debug Log';
+
+// Help
+$_['help_review_status']         = 'Orders that marked as review by FraudLabs Pro will be assigned this order status.';
+$_['help_approve_status']        = 'Orders that marked as approve by FraudLabs Pro will be assigned this order status.';
+$_['help_reject_status']         = 'Orders that marked as reject by FraudLabs Pro will be assigned this order status.';
+$_['help_email_notification']    = 'You can configure to receive email notification for the approve, review or reject action.';
+$_['help_zapier_notification']   = 'You can trigger the notification, i.e, Slack notification, using the Zapier service.';
+$_['help_simulate_ip']           = 'Simulate the visitor IP address for testing. Leave blank to disable it.';
+$_['help_debug_status']          = 'Enable or disable debug log for development purposes.';
+$_['help_fraudlabspro_id']       = 'Unique identifier for a transaction screened by FraudLabs Pro system.';
+$_['help_ip_address']            = 'IP Address.';
+$_['help_ip_usage_type']         = 'Usage type of the IP address. E.g, ISP, Commercial, Residential.';
+$_['help_ip_time_zone']          = 'Time zone of the IP address.';
+$_['help_ip_location']           = 'Location of the IP address.';
+$_['help_ip_distance']           = 'Distance from IP address to Billing Location.';
+$_['help_ip_latitude']           = 'Latitude of the IP address.';
+$_['help_ip_longitude']          = 'Longitude of the IP address.';
+$_['help_risk_country']          = 'Whether IP address country is in the latest high risk country list.';
+$_['help_free_email']            = 'Whether e-mail is from free e-mail provider.';
+$_['help_ship_forward']          = 'Whether shipping address is a freight forwarder address.';
+$_['help_using_proxy']           = 'Whether IP address is from Anonymous Proxy Server.';
+$_['help_ip_blacklist']          = 'Whether the IP address is in our blacklist database.';
+$_['help_email_blacklist']       = 'Whether the email address is in our blacklist database.';
+$_['help_phone_verify']          = 'Whether the phone number is verified by the customer.';
+$_['help_status']                = 'FraudLabs Pro status.';
+$_['help_message']               = 'FraudLabs Pro error message description.';
+$_['help_transaction_id']        = 'Unique identifier for a transaction screened by FraudLabs Pro system.';
+$_['help_credits']               = 'Balance of the credits available after this transaction.';
+$_['help_rules']                 = 'FraudLabs Pro Rules triggered.';
+
+// Error
+$_['error_permission']           = 'Warning: You do not have permission to modify FraudLabs Pro settings!';
+$_['error_key']                  = 'API Key Required!';
